6fbe33698173e3f7003fb031c4e767e06eaeed99
Wreckitshoot <billytheboybish@hotmail.com>
2024-09-15 19:44:34 -0400

    triangle

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -101,16 +101,32 @@ int main() {
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	//
-	// 0. copy our vertices array in a buffer for OpenGL to use
+	////thingy example
+	//// 0. copy our vertices array in a buffer for OpenGL to use
+	//glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//// 1. then set the vertex attributes pointers
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	//glEnableVertexAttribArray(0);
+	//// 2. use our shader program when we want to render an object
+	//glUseProgram(shaderProgram);
+	//// 3. now draw the object 
+
+	//VAO stuff
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+
+	// 1. bind Vertex Array Object
+	glBindVertexArray(VAO);
+	// 2. copy our vertices array in a buffer for OpenGL to use
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	// 1. then set the vertex attributes pointers
+	// 3. then set our vertex attributes pointers
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	// 2. use our shader program when we want to render an object
-	glUseProgram(shaderProgram);
-	// 3. now draw the object 
+
+
+	
 
 	//Render loop
 	while (!glfwWindowShouldClose(window)) {
@@ -119,6 +135,9 @@ int main() {
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		//Drawing happens here!
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
 	}
 	printf("Shutting down...");
